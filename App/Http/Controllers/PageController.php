3fe0fdf78cfa6ef2ca0e14d06e16ad69671fb245
ToyaToyas <alihusain.ridwan@gmail.com>
2024-11-17 11:30:23 +0700

    first commit

---
+++ App/Http/Controllers/PageController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function show5026221156()
+    {
+        return view('5026221156');
+    }
+
+    public function style()
+    {
+        return view('Style');
+    }
+
+    public function style2()
+    {
+        return view('Style 2');
+    }
+
+    public function form()
+    {
+        return view('form');
+    }
+
+    public function hello()
+    {
+        return view('hello');
+    }
+
+    public function tugasLinktreeRiotGames()
+    {
+        return view('tugasLinktreeRiotGames');
+    }
+    public function responsive1()
+    {
+        return view('responsive1');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+use App\Http\Controllers\PageController;
+Route::get('5026221156', [PageController::class, 'show5026221156'])->name('5026221156');
+Route::get('Style', [PageController::class, 'style'])->name('style');
+Route::get('Style 2', [PageController::class, 'style2'])->name('style2');
+Route::get('form', [PageController::class, 'form'])->name('form');
+Route::get('hello', [PageController::class, 'hello'])->name('hello');
+Route::get('tugasLinktreeRiotGames', [PageController::class, 'tugasLinktreeRiotGames'])->name('tugasLinktree');
+Route::get('responsive1', [PageController::class, 'responsive1'])->name('responsive1');
+
+use App\Http\Controllers\PegawaiDBController;
+Route::get('/pegawai', [PegawaiDBController::class, 'index']);
+Route::get('/pegawai/tambah', [PegawaiDBController::class, 'tambah']);
+Route::post('/pegawai/store', [PegawaiDBController::class, 'store']);
